d113122b5bd5e9b9d416b237fe89a25f2633521b
Praveen <praveenrijal.23it@licet.ac.in>
2021-07-19 23:39:59 +0530

    FInal

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 
-	$conn = mysqli_connect('localhost','root','','sparks_bank');
+$conn = mysqli_connect('localhost', 'id17276602_killer2002', 'J+?Psa^eP-~PH$>9', 'id17276602_anonymousfrand');
 
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
+if (!$conn) {
+	die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
+}
